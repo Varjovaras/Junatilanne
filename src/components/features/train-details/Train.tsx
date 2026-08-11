4affd05cfe9db4fafd20989bd4a45f367effeb64
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2026-08-11 19:38:11 +0300

    change /trains/ list of stations border

--- src/components/features/train-details/Train.tsx
+++ src/components/features/train-details/Train.tsx
@@ -15,7 +15,7 @@ const Train = ({ train }: TrainProps) => {
     return (
         <div
             key={`train-${train.trainNumber}`}
-            className="border border-double border-red-600 p-4 m-2 overflow-hidden wrap-break-word flex flex-col min-h-[200px]"
+            className="border border-border bg-surface rounded-lg px-4 py-3 m-2 overflow-hidden wrap-break-word flex flex-col min-h-[200px]"
         >
             <TrainButton train={train} />
             <div className="flex-1 flex flex-col">
